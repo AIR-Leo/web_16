58ab92c6bf357cf5df269c7089e6cb3f12d2cd5e
AIR-Leo <info@leonardoguerra.com>
2023-07-20 17:23:42 +0200

    UPD Cina

--- web_pivot_computed_measure/static/src/dropdown_item_custom_measure/dropdown_item_custom_measure.esm.js
+++ web_pivot_computed_measure/static/src/dropdown_item_custom_measure/dropdown_item_custom_measure.esm.js
@@ -3,7 +3,7 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html) */
 
 const {Component, QWeb} = owl;
-const {useState} = owl.hooks;
+const {useState} = owl;
 
 /**
  * @extends Component
